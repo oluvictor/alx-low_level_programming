17ecd1b900b02fd142bf2959a30102fa2598a53a
Victor Ogunshola <vogunshola@gmail.com>
2022-06-30 20:41:36 -0700

    fixed 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,10 +6,10 @@
  *
  * Return 0
  */
-int main (void)
+int main(void)
 {
 	char string[] = "\"Programming is like building a multilingual puzzle";
+	
 	puts(string);
-
-	return 0;
+	return (0);
 }
