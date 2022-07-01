3d861b8639f41372560c55a1b757784f3a2b50bd
Victor Ogunshola <vogunshola@gmail.com>
2022-06-30 21:07:44 -0700

    added anf fixed 4 and 5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
-#include <string.h>
 
 /**
- * Displays a string to output using puts
+ * main - Entry point
  *
- * Return 0
+ * Return: 0 (Always success)
  */
 int main(void)
 {
 	char string[] = "\"Programming is like building a multilingual puzzle";
-	
+
 	puts(string);
 	return (0);
 }
