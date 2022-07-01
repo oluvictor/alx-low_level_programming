663fc1bb1292353de84dd9924621f076e084b649
Victor Ogunshola <vogunshola@gmail.com>
2022-06-30 20:37:04 -0700

    added 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * Displays a string to output using puts
+ *
+ * Return 0
+ */
+int main (void)
+{
+	char string[] = "\"Programming is like building a multilingual puzzle";
+	puts(string);
+
+	return 0;
+}
