232003edd6ba3fd800b208882114029b6c3f0618
Victor Ogunshola <vogunshola@gmail.com>
2022-06-30 21:56:43 -0700

    added 101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+
+	fprintf(stderr, "%s\n", str);
+	return (1);
+}
