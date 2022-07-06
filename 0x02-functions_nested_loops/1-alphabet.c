e5c45fae2b8a6520e7c81c5e7c0531083367a183
Victor Ogunshola <vogunshola@gmail.com>
2022-07-05 19:47:27 -0700

    added task 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+/**
+ * print_alphabet - print all lowercase alphabets
+ *
+ * Return: 0 (always success)
+ */
+void print_alphabet(void)
+{
+	int i = 97;
+
+	while (i <= 122)
+	{
+		_putchar(i);
+	}
+	_putchar(10);
+}
