e31bea565edd9c67b9d94c4ab5a529e8d1220da4
Victor Ogunshola <vogunshola@gmail.com>
2022-07-05 21:14:37 -0700

    added 7-print_last_digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,23 @@
+#include <unistd.h>
+#include "main.h"
+/**
+ * print_last_digit - prints the last digit
+ * @n: the number to be printed
+ *
+ * Return: last digit of n
+ */
+int print_last_digit(int n)
+{
+	int last;
+
+	if (n == 0)
+	{
+		_putchar(48);
+		return (0);
+	}
+	else
+	{
+		last  = n % 10;
+		return (last);
+	}
+}
