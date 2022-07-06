25e747efa7c0469bd562a56a398fdf24e9381331
Victor Ogunshola <vogunshola@gmail.com>
2022-07-05 20:51:05 -0700

    added print-sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_sign - checks if a number is positive, negative
+ * or zero
+ * @n: the number to be checked
+ *
+ * Return: 1 if positive, 0 if 0, and -1 if negative
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(48);
+		return (0);
+	}
+	else if (n < 0)
+	{
+		_putchar(45);
+		return (-1);
+	}
+}
