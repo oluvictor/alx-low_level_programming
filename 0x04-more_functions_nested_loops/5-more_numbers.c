a2edbafa163d3bc43424e99fd0edb46605d984e0
Victor Ogunshola <vogunshola@gmail.com>
2022-07-08 22:01:20 -0700

    h

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,16 +8,19 @@
  */
 void more_numbers(void)
 {
-	int i;
+	int i, j;
 
+	for (j =  1; j <= 10; j++)
+	{
 	for (i = 0; i <= 14; i++)
 	{
-		if (i < 10)
+		if (i >= 10)
 			_putchar((i / 10) + '0');
 		_putchar((i % 10) + '0');
 		if (i == 14)
 		{
 			_putchar('\n');
 		}
+	}
 	}
 }
