3e0bfce052fc5d72996affa42f623b4c086b3253
Victor Ogunshola <vogunshola@gmail.com>
2022-07-08 18:04:28 -0700

    added 2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _isdigit - multiplies 2 integers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: result of multiplication
+ */
+int _isdigit(int a, int b)
+{
+	return (a * b);
+}
