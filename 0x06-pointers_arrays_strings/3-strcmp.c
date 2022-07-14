3f0cf1641cd2fda5a8bdace051c2103ad0f53acf
Victor Ogunshola <vogunshola@gmail.com>
2022-07-14 15:59:43 -0700

    fixed strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -16,13 +16,13 @@ int _strcmp(char *s1, char *s2)
 
 	while (*(s1 + i) != '\0')
 	{
-		s1_size += *(s1 + i);
+		s1_size++; /* += *(s1 + i);*/
 		i++;
 	}
 	i = 0;
 	while (*(s2 + i) != '\0')
 	{
-		s2_size += *(s2 + i);
+		s2_size++; /* *(s2 + i);*/
 		i++;
 	}
 	if (s1_size < s2_size)
