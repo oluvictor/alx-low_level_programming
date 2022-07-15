a0d19c2eac870949c7f23fa6399433f4db6f906f
Victor Ogunshola <vogunshola@gmail.com>
2022-07-14 17:03:02 -0700

    fixed string_toupper

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -9,10 +9,11 @@
 char *string_toupper(char *s)
 {
 	int i = 0;
+
 	while (*(s + i) != '\0')
 	{
 		if ((*(s + i) >= 97) && (*(s + i) <= 121))
-			*(s + i) -= 26;
+			*(s + i) -= 32;
 		i++;
 	}
 	return (s);
