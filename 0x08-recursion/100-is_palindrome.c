146f402f92d790105402ad9ad0c68c04eaa45cea
Victor Ogunshola <vogunshola@gmail.com>
2022-07-20 22:05:35 -0700

    fixed betty doc in is_palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -2,7 +2,7 @@
 #include <stddef.h>
 
 /**
- * is_pelindrome - returns 1 if a string is a palindrome
+ * is_palindrome - returns 1 if a string is a palindrome
  * and 0 if not
  * @s: pointer to string
  *
