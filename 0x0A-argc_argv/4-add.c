a0fe5961edbcc7fbdfa6658468b4224d3a3b1bb6
Victor Ogunshola <vogunshola@gmail.com>
2022-07-27 00:29:56 -0700

    fixed error in non-numeric characters in 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -10,19 +10,21 @@
  */
 int main(int argc, char *argv[])
 {
-	int i, sum = 0;
+	int i, j, sum = 0;
 
 	if (argc > 0)
 	{
 		for (i = 1; i < argc; i++)
 		{
-			if (*argv[i] >= '0' && *argv[i] <= '9')
-				sum += atoi(argv[i]);
-			else
+			for (j = 0; argv[i][j] != '\0'; j++)
 			{
-				printf("Error\n");
-				return (1);
+				if (!(argv[i][j] >= '0' && argv[i][j] <= '9'))
+				{
+					printf("Error\n");
+					return (1);
+				}
 			}
+			sum += atoi(argv[i]);
 		}
 		printf("%d\n", sum);
 		return (0);
