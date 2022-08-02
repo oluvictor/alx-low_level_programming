5c2f810ac14a293b1ae8116df00c0241f1afc6c7
Victor Ogunshola <vogunshola@gmail.com>
2022-08-01 19:21:28 -0700

    added a dog struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+/**
+ * struct dog - a dog data type
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: string owner of dog
+ *
+ * Description: This structure will help me get all the properties
+ * of a dog in one identifier.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
