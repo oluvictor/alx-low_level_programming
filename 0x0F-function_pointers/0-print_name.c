696928873ca194d8b49558ea49faf11be9485ad0
Victor Ogunshola <vogunshola@gmail.com>
2022-08-03 21:14:06 -0700

    created the print_name function

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name
+ * @name: name to be printed
+ * @f: pointer to function
+ *
+ * Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	/*(*f)(name);*/
+	f(name);
+}
