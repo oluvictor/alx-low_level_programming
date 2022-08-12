04804c6d0d8193e7f846737ff68f71ad04c8c4ff
Victor Ogunshola <vogunshola@gmail.com>
2022-08-11 21:40:00 -0700

    added function free list

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -3,18 +3,19 @@
 
 /**
  * free_list - frees a list_t list
- * @h: a list
+ * @head: a list
  *
  * Return: void
  */
 void free_list(list_t *head)
 {
 	list_t *temp;
+
 	while (head != NULL)
 	{
-			temp = head;
-			head = head->next;
-			free(temp->str);
-			free(temp);
+		temp = head;
+		head = head->next;
+		free(temp->str);
+		free(temp);
 	}
 }
