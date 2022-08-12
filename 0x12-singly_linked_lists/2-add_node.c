408040bddf46248d23596cacad12ac1f05574867
Victor Ogunshola <vogunshola@gmail.com>
2022-08-11 20:20:21 -0700

    fixed add_node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -13,16 +13,19 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new;
-	
+	size_t nchar;
+
 	new = malloc(sizeof(list_t));
 	if (new != NULL)
 	{
 		new->str = strdup(str);
-		new->len = strlen(str);
-		if (*head == NULL)
-			new->next = NULL;
-		else
-			new->next = *head;
+
+		for (nchar = 0; str[nchar]; nchar++)
+			;
+
+		new->len = nchar;
+		new->next = *head;
+		*head = new;
 		return (new);
 
 	}
