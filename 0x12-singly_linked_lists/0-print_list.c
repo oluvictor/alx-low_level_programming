44dd60d230bed6fa0f14742ab7a49ed8b380dc91
Victor Ogunshola <vogunshola@gmail.com>
2022-08-11 20:25:49 -0700

    fixed output of print_list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,20 +12,16 @@ size_t print_list(const list_t *h)
 {
 	size_t size = 0;
 
-	if (h != NULL)
+	while (h != NULL)
 	{
-		while (1)
-		{
-			if (h->str == NULL)
-				printf("[0] (nil)\n");
-			else
-				printf("[%d] (%s)\n", h->len, h->str);
-			size++;
-			if (h->next == NULL)
-				break;
-			h = h->next;
-		}
-
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		size++;
+		if (h->next == NULL)
+			break;
+		h = h->next;
 	}
 	return (size);
 }
