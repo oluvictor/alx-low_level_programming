8bc62f6996ed9ee5b78a5781bb54cfd096f68d0d
Victor Ogunshola <vogunshola@gmail.com>
2022-08-11 19:14:32 -0700

    created print_list function

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_list - prints all the elements of a list_t list
+ * @h: a list
+ *
+ * Returns: number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	size_t size = 0;
+
+	if (h != NULL)
+	{	
+		while (1)
+		{
+			if (h->str == NULL)
+				printf("[0] (nil)\n");
+			printf("[%d] (%s)\n", h->len, h->str);
+			size++;
+			if (h->next == NULL)
+				break;
+			h = h->next;
+		}
+
+	}
+	return (size);
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,4 @@
+##0x12-singly_linked_lists
+#Learning Objectives
+- When and why using linked lists vs arrays
+- How to build and use linked lists
