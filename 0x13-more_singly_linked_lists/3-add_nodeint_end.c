c1f907d7bfc17dcca106278019fbd8e8018f9c8e
Victor Ogunshola <vogunshola@gmail.com>
2022-08-16 17:48:05 -0700

    added function that adds node to end of list

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * add_nodeint_end - adds a new node at the end of a listint_t list
+ * in a linked listint_t list
+ * @head: list
+ * @n: int for the new list
+ *
+ * Return: pointer to the new head
+ */
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *new;
+	listint_t *temp;
+
+	new  = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	
+	new->n = n;
+	new->next = NULL;
+	temp = *head;
+
+	if (*head == NULL)
+	{
+		*head = new;
+	}
+	else
+	{
+		while (temp->next != NULL)
+			temp = temp->next;
+		temp->next = new;
+	}
+
+	return (new);
+}
